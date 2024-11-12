c8374fa08f1cbeff15ffd6ec5aa76c4408aa19a9
shekhar-11 <rajshekhar111201@gmail.com>
2024-11-12 20:36:50 +0530

    database connected

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+const url = process.env.MONGO;
+// console.log(typeof url);
+// console.log(typeof url);
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 const app = express();
 app.get("/", (req, res) => {
-  res.send({ message:"Hello" });
+  res.send({ message: "Hello" });
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
