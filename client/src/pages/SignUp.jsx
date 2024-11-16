9bb15743ceabfe3a0368c5155565192ff8f386a9
shekhar-11 <rajshekhar111201@gmail.com>
2024-11-16 11:54:26 +0530

    sign up page ui creation

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "react-router-dom";
+import { Link } from "react-router-dom";
 export default function SignUp() {
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -30,7 +30,7 @@ export default function SignUp() {
       <div className="flex gap-2 mt-5">
         <p>Have an acocunt?</p>
         <Link to={"/sign-in"}>
-          <span className="text-blue-700">SIgn In</span>
+          <span className="text-blue-700">Sign In</span>
         </Link>
       </div>
     </div>
