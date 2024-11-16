3bcccecc759033b716016e7db7e2bd746fc480ba
shekhar-11 <rajshekhar111201@gmail.com>
2024-11-16 11:21:29 +0530

    create signup with auth and pass hash

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
+// import dotenv from "dotenv";
 import dotenv from "dotenv";
-dotenv.config();
+
+// Load the .env file from the root
+dotenv.config({ path: "../.env" });
+
+// dotenv.config();
+import authRouter from "./routes/auth.route.js";
+
 import userRouter from "./routes/user.route.js";
 const url = process.env.MONGO;
 // console.log(typeof url);
@@ -11,6 +18,8 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 const app = express();
+
+app.use(express.json()); //it allows the user to send the json format input
 app.get("/", (req, res) => {
   res.send({ message: "Hello" });
 });
@@ -22,3 +31,5 @@ app.use("/api/user", userRouter);
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/auth", authRouter);
