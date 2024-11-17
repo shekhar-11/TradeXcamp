0830652d7689dd18d5146824dc30b86a23669af2
shekhar-11 <rajshekhar111201@gmail.com>
2024-11-17 11:22:52 +0530

    signin API route

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,7 +1,7 @@
 //handles the error
 
 export const errorHandler = (statusCode, message) => {
-  const error = new Error(message);
+  const error = new Error();
   error.statusCode = statusCode;
   error.message = message;
   return error;
